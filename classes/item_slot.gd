c81b94a0980309840dfaaf27cb79e0d880ab827c
hawk0f <sokolovm107@gmail.com>
2024-01-22 15:27:43 +0300

    Выбор слота сопровождается изменением иконки (текущий (выбранный) слот имеет белую иконку

--- classes/item_slot.gd
+++ classes/item_slot.gd
@@ -15,10 +15,22 @@ func update(slot: InventorySlot):
 		itemsprite.visible = true
 		itemsprite.texture = slot.item.texture
 
+func update_bottom_inventory_slot(slot: InventorySlot):
+	if !slot.item:
+		itemsprite.visible = false
+		amountlabel.visible = false
+	else:
+		amountlabel.text = str(slot.amount)
+		amountlabel.visible = true
+		
+		backgroundSprite.texture = slot.texture
+		
+		itemsprite.visible = true
+		itemsprite.texture = slot.item.texture
+  
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
