b483bba7f2bb4dc1de8ca37c4f2c4f62713af22c
veydoge <125701786+veydoge@users.noreply.github.com>
2024-01-27 16:03:31 +0300

    open/close inventory bug on different scenes fixed

--- CanvasLayer.gd
+++ CanvasLayer.gd
@@ -4,7 +4,13 @@ extends CanvasLayer
 
 func _input(event):
 	if (event.is_action_pressed("toggle_inventory")):
-		if (get_node("../Area2D/shop") as Shop).isShopOpen == false && (get_node("../chest/workbench") as Workbench).isWorkbenchOpen == false:
+		if (get_node("../Area2D/shop") as Shop) && (get_node("../chest/workbench") as Workbench): # if shop and workbench exists
+			if (get_node("../Area2D/shop") as Shop).isShopOpen == false && (get_node("../chest/workbench") as Workbench).isWorkbenchOpen == false:
+				if inventory.isOpen:
+					inventory.close()
+				else:
+					inventory.open()
+		else:
 			if inventory.isOpen:
 				inventory.close()
 			else:
